69bb10ab1b515620d0a3e1bae32badc80ecb4348
Devison600 <cauadevison@gmail.com>
2024-10-21 09:13:52 -0300

    Add files via upload

---
+++ projeto sobre estado civil.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+char letra;
+printf("digite a letra:");
+scanf("%c",&letra);
+if(letra =='s'){
+printf("solteiro");
+}
+if(letra =='c'){
+printf("casado");
+}
+if(letra =='d'){
+printf("divorciado");
+}
+if(letra =='v'){
+printf("viuvo");
+}
+}
+
+
+
+
+
